0071c956a401038082b207075922b66110f68cda
jlumayan-clc <joaquin.sebastianumayan@gmail.com>
2026-08-16 22:48:40 +0800

    Fix birthdate input and calculate Chinese Zodiac
    Corrected variable name and added zodiac sign calculation.

--- q1/q1_sg2_a3.py
+++ q1/q1_sg2_a3.py
@@ -1,5 +1,25 @@
 birthdate = int(input("Enter your birth year: "))
+# User inputs birth year
 
 if birthdate < 1900:
   print("Invalid year, it should not be earlier than 1900")
-elif 
+  # Error message if birthdate is less than 1900
+else:
+  zodiacs = ["Rat (鼠 / Shǔ)",
+  "Ox (牛 / Niú)",
+  "Tiger (虎 / Hǔ)",
+  "Rabbit (兔 / Tù)",
+  "Dragon (龙 / Lóng)",
+  "Snake (蛇 / Shé)",
+  "Horse (马 / Mǎ)",
+  "Goat (羊 / Yáng)",
+  "Monkey (猴 / Hóu)",
+  "Rooster (鸡 / Jī)",
+  "Dog (狗 / Gǒu)",
+  "Pig (猪 / Zhū)"]
+  # The zodiacs
+
+zodiacsign = (birthyear - 1900)%12
+#1900 is subtracted, the difference is divided by 12, and the remainder is the index the zodiac is in
+
+print(f"Your Chinese Zodiac Sign is : {zodiacs[zodiacsign]}")
